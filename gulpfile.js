7e3e346947d722fac1f5557e0dee9360b17a81b4
happo31 <happo31@gmail.com>
2018-02-23 01:35:20 +0900

    コマンド名修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,22 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
 const webpack = require("webpack");
 const webpackStream = require("webpack-stream");
 const watch = require("gulp-watch");
 
-const webpackConfigServer = require('./webpack-config/webpack.server.config');
-const webpackConfigClient = require('./webpack-config/webpack.client.config');
+const webpackConfigServer = require("./webpack-config/webpack.server.config");
+const webpackConfigClient = require("./webpack-config/webpack.client.config");
 
-gulp.task("compile:client", ()=> {
-    return webpackStream(webpackConfigClient, webpack)
-        .pipe(gulp.dest("dest/client/"));
+gulp.task("build:client", () => {
+  webpackStream(webpackConfigClient, webpack).pipe(gulp.dest("dest/client/"));
 });
 
-gulp.task("compile:server", ()=> {
-    return webpackStream(webpackConfigServer, webpack)
-        .pipe(gulp.dest("dest/server/"));
+gulp.task("build:server", () => {
+  webpackStream(webpackConfigServer, webpack).pipe(gulp.dest("dest/server/"));
 });
 
-gulp.task("default", [
-    "compile:server",
-    "compile:client",
-]);
+gulp.task("build", ["build:server", "build:client"]);
 
-gulp.task("watch", ["default"], ()=> {
-    gulp.watch(["src/client/**/*"], ["compile:client"]);
-    gulp.watch(["src/server/**/*"], ["compile:server"]);
+gulp.task("watch", ["build"], () => {
+  gulp.watch(["src/client/**/*"], ["build:client"]);
+  gulp.watch(["src/server/**/*"], ["build:server"]);
 });
